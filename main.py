8dc63ba666887f56ad44b1dac2d61a9632144bfe
DVNguyen <131824809+vnguyen31@users.noreply.github.com>
2024-04-10 22:16:53 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,28 +5,38 @@ from scipy import signal
 from scipy.io import wavfile
 
 #Function to open the .wav file and extract its sampling frequency, array of samples
-def DTFT(wav_file):
+def DTFT(wav_file, newsamplingrate):
     #read wav file, print sampling rate and number of samples
     sampling_rate, samples = wavfile.read(wav_file)
-    print("Sampling Rate:", sampling_rate)
+    print("Original Sampling Rate:", sampling_rate)
     print("Number of Samples:", len(samples))
 
     #modify the sample rate
-    fsample = 3 * sampling_rate
+    if newsamplingrate < sampling_rate:
+        sample_factor = int(sampling_rate / newsamplingrate)
+        #slicing array
+        newsamples = samples[::sample_factor]
+    else:
+        #no downsampling needed
+        newsamples = samples
+
+    #update sampling rate
+    fsample = newsamplingrate
 
     #Discrete Time function of song
-    taxis = np.arange(0 , len(samples), 1)
+    num_samples = len(newsamples)
+    taxis = np.linspace(0, num_samples / sampling_rate, num_samples)
 
     #DTFT of the samples in .wav file
-    Xomega = np.fft.fft(samples)
+    Xomega = np.fft.fft(newsamples)
     #|Xomega| = absolute value of Xomega
     Xomega_magnitude = np.abs(Xomega)
     #Freqeuncy axis: array of floats of frequency
-    faxis = np.fft.fftfreq(len(samples), d = 1/fsample)
+    faxis = np.fft.fftfreq(len(newsamples), d = 1/fsample)
 
     #plotting x[n] with respect to its samples (integers)
     plt.figure(figsize=(20, 12))
-    plt.plot(taxis, samples, label='Digital samples of song')
+    plt.plot(taxis, newsamples, label='Digital samples of song')
     plt.title('Samples of Song')
     plt.xlabel('Samples [n]')
     plt.ylabel('Magnitude x[n]')
@@ -47,4 +57,4 @@ def DTFT(wav_file):
     plt.autoscale()
 
 #python thinks 1 \t is tab, use raw string
-DTFT(r"C:\Users\ducvi\PycharmProjects\S&S 1.94\.venv\thuong em.wav")
+DTFT(r"C:\Users\ducvi\PycharmProjects\S&S 1.94\.venv\thuong em.wav", 1000)

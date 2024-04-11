2c998aafdd1746a110e28dce91e9789f8161f6c2
DVNguyen <131824809+vnguyen31@users.noreply.github.com>
2024-04-10 20:13:31 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+#includes
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import signal
+from scipy.io import wavfile
+
+#Function to open the .wav file and extract its sampling frequency, array of samples
+def DTFT(wav_file):
+    #read wav file, print sampling rate and number of samples
+    sampling_rate, samples = wavfile.read(wav_file)
+    print("Sampling Rate:", sampling_rate)
+    print("Number of Samples:", len(samples))
+
+    #modify the sample rate
+    fsample = 3 * sampling_rate
+
+    #Discrete Time function of song
+    taxis = np.arange(0 , len(samples), 1)
+
+    #DTFT of the samples in .wav file
+    Xomega = np.fft.fft(samples)
+    #|Xomega| = absolute value of Xomega
+    Xomega_magnitude = np.abs(Xomega)
+    #Freqeuncy axis: array of floats of frequency
+    faxis = np.fft.fftfreq(len(samples), d = 1/fsample)
+
+    #plotting x[n] with respect to its samples (integers)
+    plt.figure(figsize=(20, 12))
+    plt.plot(taxis, samples, label='Digital samples of song')
+    plt.title('Samples of Song')
+    plt.xlabel('Samples [n]')
+    plt.ylabel('Magnitude x[n]')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    plt.autoscale()
+
+    #plotting the DTFT of x[n] in frequency domainn
+    plt.figure(figsize = (20, 12))
+    plt.plot(faxis, Xomega_magnitude, label = 'Frequencies in .wav file')
+    plt.title('Frequency Spectrum')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('|X(omega)|')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    plt.autoscale()
+
+#python thinks 1 \t is tab, use raw string
+DTFT(r"C:\Users\ducvi\PycharmProjects\S&S 1.94\.venv\thuong em.wav")
